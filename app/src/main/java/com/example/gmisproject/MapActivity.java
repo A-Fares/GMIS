2ce08523b2ab47575fd1777b76c9cd62263f7deb
Osama Amin <osamaamin141@gmail.com>
2020-03-08 20:09:11 +0200

    the back icon in map activity back to its parent activity once you click on it

--- app/src/main/java/com/example/gmisproject/MapActivity.java
+++ app/src/main/java/com/example/gmisproject/MapActivity.java
@@ -1,6 +1,9 @@
 package com.example.gmisproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,14 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        ImageView child = findViewById(R.id.image_view_back);
+        child.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              startActivity(getParentActivityIntent());
+              
+            }
+        });
     }
 }
